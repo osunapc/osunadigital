90ea79cb0e313d2acd41924cc4fc3b70b46c8c07
osunapc <osunapc@gmail.com>
2024-10-09 13:13:11 -0400

    first commit

---
+++ src/projects.js
@@ -0,0 +1,34 @@
+export const Projects = [
+	{
+		id: 1,
+		img: "/img/mtl.jpg",
+		logo: "/img/mtl-logo.jpg",
+		alt: "Multlock ve",
+		github: "https://github.com/osunapc/multlockve",
+		link: "https://multlockve.netlify.app/",
+	},
+	{
+		id: 2,
+		img: "/img/turisteando.jpg",
+		logo: "/img/tu-logo.jpg",
+		alt: " turisteando en puerto rico",
+		github: "https://github.com/osunapc/turistiandoenpuestorico",
+		link: "https://turisteandoenpuertorico.netlify.app",
+	},
+	{
+		id: 3,
+		img: "/img/cariberentals.jpg",
+		logo: "/img/cari-logo.jpg",
+		alt: "cariberentals",
+		github: "https://caribbeanrentalspr.com/o",
+		link: "https://caribbeanrentalspr.com/p",
+	},
+	{
+		id: 4,
+		img: "/img/relinda.jpg",
+		logo: "/img/re-logo.jpg",
+		alt: "Relinda vnzla",
+		github: "hhttps://github.com/osunapc/relindavnzla",
+		link: "https://relindavnzla.netlify.app/",
+	},
+]; 
